c58c5b07b0cd7b9f24e00a3c3100a3d4f8fab38a
neonfighter28 <58990354+neonfighter28@users.noreply.github.com>
2020-09-02 16:26:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import re
 import math
 
-TOKEN = "pleaseinsertyourtokenherethankyou"
+TOKEN = "insertyourtokenhere"
 class DiscordClient(discord.Client):
 
     async def on_ready(self):
@@ -15,7 +15,12 @@ class DiscordClient(discord.Client):
             return 
         print("  Received a message: [", message.content, "]")
         m = re.search("[\d]*[!]", message.content)
-        s = "I didn't expect this factorial! Did you really mean '" + str(math.factorial(int(m[0][:-1]))) + "' ?"
+        try:
+            f = format(math.factorial(int(m[0][:-1])), "E")
+            s = "I didn't expect this factorial! Did you really mean '" + f + "' ?"
+        except OverflowError:
+            s = "Your factorial made my Integer overflow :/"
+        
         if m != None:
             await message.channel.send(s)
 
